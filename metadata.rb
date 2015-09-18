bb27a93c4f6fc3dc39b13c3a037f4fb70ed9af11
Tim Smith <tsmith84@gmail.com>
2015-09-18 10:28:27 -0700

    Update company name and add supermarket metadata

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,12 @@
-name             'chef-analytics'
-maintainer       'Chef Inc.'
+name 'chef-analytics'
+maintainer 'Chef Software, Inc.'
 maintainer_email 'edavis@chef.io'
-license          'All rights reserved'
-description      'Installs/Configures chef-analytics'
+license 'All rights reserved'
+description 'Installs/Configures chef-analytics'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version '0.1.0'
 
 depends 'chef-ingredient', '>= 0.7.0'
+
+source_url 'https://github.com/chef-cookbooks/chef-analytics' if respond_to?(:source_url)
+issues_url 'https://github.com/chef-cookbooks/chef-analytics/issues' if respond_to?(:issues_url)
