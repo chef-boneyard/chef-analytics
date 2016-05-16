1d66c46788e3fd709a47266abcb7589db426849d
Bakh Inamov <b@chef.io>
2016-05-16 16:58:27 -0700

    adds accept_license attribute
    this all is @mengesb work out of https://github.com/chef-cookbooks/chef-analytics/pull/6 that pertains to accept_license. I removed the libraries/helper.rb from this commit.
    
    fixes #5

--- recipes/default.rb
+++ recipes/default.rb
@@ -18,6 +18,7 @@ chef_ingredient 'analytics' do
   channel node['chef-analytics']['channel'].to_sym
   version node['chef-analytics']['version']
   package_source node['chef-analytics']['package_source']
+  accept_license node['chef-analytics']['accept_license'] unless node['chef-analytics']['accept_license'].nil?
   action :upgrade
 end
 
