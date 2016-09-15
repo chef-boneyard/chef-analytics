18a35133f14d7a37752f09018390a3a58994d3f5
Tim Smith <tsmith@chef.io>
2016-09-15 16:16:39 -0700

    Remove chef 11 compatibility in the metadata
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -15,4 +15,4 @@ end
 source_url 'https://github.com/chef-cookbooks/chef-analytics'
 issues_url 'https://github.com/chef-cookbooks/chef-analytics/issues'
 
-chef_version '>= 12.1' if respond_to?(:chef_version)
+chef_version '>= 12.1'
